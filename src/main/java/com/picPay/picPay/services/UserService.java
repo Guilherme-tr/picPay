c0afd5375703a481a6f785ff9039f5e798e223b8
Guilherme-tr <gui.trujilo.wecomp@gmail.com>
2024-11-27 21:16:47 -0300

    feat: create getAllUsers.

--- src/main/java/com/picPay/picPay/services/UserService.java
+++ src/main/java/com/picPay/picPay/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -36,5 +37,9 @@ public class UserService {
         User newUser = new User(data);
         this.saveUser(newUser);
         return newUser;
+    }
+
+    public List<User> getAllUsers(){
+        return this.userRepository.findAll();
     }
 }
